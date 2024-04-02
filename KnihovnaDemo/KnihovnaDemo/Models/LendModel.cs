ffd9267e1ed6bed7b82d0590b955513720a9f32c
RadimHruska <hruska@bbnsro.info>
2024-04-03 00:56:06 +0200

    lend model

---
+++ KnihovnaDemo/KnihovnaDemo/Models/LendModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnihovnaDemo.Models
+{
+    public class LendModel
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public int IdBook { get; set; }
+        public string NameOfBook { get; set; }
+        public string NameOfUser { get; set;}
+        public DateTime LandedDate { get; set; }
+        public DateTime? ReturnedDate { get; set;}
+
+    }
+}
